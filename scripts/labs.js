2608c0eee2fbecef1a95679fedcd46247c6d99f8
kivanov82 <kivanov82@yahoo.com>
2024-11-06 10:54:56 +0100

    First version of Labs deployment script

---
+++ scripts/labs.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const labsRegistry = await hre.ethers.deployContract("LabsRegistry", [])
+  await labsRegistry.waitForDeployment();
+  console.log(
+    `LabsRegistry was deployed to ${await labsRegistry.getAddress()}`
+  );
+
+  //verify
+  if (hre.network.name !== 'localhost') {
+    console.log('Waiting before verification....')
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    await delay(15000);
+
+    await hre.run("verify:verify", {
+      address: await labsRegistry.getAddress()
+    });
+  }
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
